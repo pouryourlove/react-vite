55551df843f6246f7d4b4532360cfaa3e9cf3a9f
Juyeon Lee <lqu134@naver.com>
2023-05-26 15:41:02 +0900

    non-string props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import Chicken from "./Chicken";
 import Greeter from "./Greeter";
+import Die from "./Die";
 
 function App() {
   return (
     <div>
-      <Greeter person="Bill" from="colt" />
+      {/* <Greeter person="Bill" from="colt" />
       <Greeter person="Julia" from="colt" />
-      <Greeter person="Ted" from="Elton"/>
+      <Greeter person="Ted" from="Elton" /> */}
+      <Die numSides={20} />
+      <Die numSides={6} />
+      <Die numSides={10} />
     </div>
   );
 }
