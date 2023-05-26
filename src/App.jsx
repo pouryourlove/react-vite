5d1415ec82646e2978d30c4182c582636cb70d1b
Juyeon Lee <lqu134@naver.com>
2023-05-26 16:23:30 +0900

    react conditional

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import "./App.css";
 import Chicken from "./Chicken";
 import Greeter from "./Greeter";
 import Die from "./Die";
+import ListPicker from "./ListPicker";
+import DoubleDice from "./DoubleDice";
 
 function App() {
   return (
     <div>
+      <DoubleDice />
+      <DoubleDice />
+      <DoubleDice />
+      {/* <ListPicker values={[1, 2, 3]} />
+      <ListPicker values={["a", "b", "c"]} /> */}
       {/* <Greeter person="Bill" from="colt" />
-      <Greeter person="Julia" from="colt" />
-      <Greeter person="Ted" from="Elton" /> */}
+      <Greeter person="Julia" />
+      <Greeter />
       <Die numSides={20} />
-      <Die numSides={6} />
-      <Die numSides={10} />
+      <Die />
+      <Die numSides={10} /> */}
     </div>
   );
 }
