dc1d6495d2ca8c0aed173eabac71f1c9b2176bbe
Juyeon Lee <lqu134@naver.com>
2023-05-30 22:46:36 +0900

    created a shopping list component

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,22 @@ import DoubleDice from "./DoubleDice";
 import Heading from "./Heading";
 import ColorList from "./ColorList";
 import Slots from "./Slots";
+import ShoppingList from "./ShoppingList";
+
+const data = [
+  { item: "eggs", quantity: 12, completed: false },
+  { item: "milk", quantity: 1, completed: true },
+  { item: "chicken breasts", quantity: 4, completed: false },
+  { item: "carrots", quantity: 6, completed: true },
+];
 
 function App() {
   return (
-    <div>
-      <Slots val1="🎈" val2="🎈" val3="🎈" />
-      <Slots val1="🎈" val2="🎃" val3="🎈" />
-    </div>
+    <ShoppingList items={data} />
+    // <div>
+    //   <Slots val1="🎈" val2="🎈" val3="🎈" />
+    //   <Slots val1="🎈" val2="🎃" val3="🎈" />
+    // </div>
   );
 }
 
