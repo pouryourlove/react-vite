439a2404e88f120a4787a9036ec30644e237f916
Juyeon Lee <lqu134@naver.com>
2023-06-01 22:03:47 +0900

    ShoppingListItem component added

--- src/ShoppingList.jsx
+++ src/ShoppingList.jsx
@@ -1,16 +1,16 @@
+import ShoppingListItem from "./ShoppingListItem";
+
 function ShoppingList({ items }) {
   return (
     <ul>
       {items.map((i) => (
-        <li
+        <ShoppingListItem
           key={i.id}
-          style={{
-            color: i.completed ? "gray" : "red",
-            textDecoration: i.completed ? "line-through" : "none",
-          }}
-        >
-          {i.item} - {i.quantity}
-        </li>
+          item={i.item}
+          quantity={i.quantity}
+          completed={i.completed}
+          // <ShoppingListItem key={i.id} {...i} />
+        />
       ))}
     </ul>
   );
