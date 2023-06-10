1af8ec2285303ed601f158bbe00cdf6cdab542cc
Juyeon Lee <lqu134@naver.com>
2023-06-10 16:42:55 +0900

    Right way to use useState

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,11 +3,6 @@ import { useState } from "react";
 export default function Counter() {
 <<<<<<< HEAD
   const [num, setNum] = useState(0);
-=======
-  const [num, setNum] = useState(5);
-  console.log("COMPONENT HAS BEEN EXECUTED AGAIN!");
-  console.log(`num: ${num}`);
->>>>>>> ab7327da6999809bbe163e8ff963b898e95df625
 
   const changeNum = () => {
     setNum(num + 1);
