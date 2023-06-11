66a74f632c052a7ed73f05a469879ef6d98e591a
Juyeon Lee <lqu134@naver.com>
2023-06-11 20:49:28 +0900

    react re-rendering

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 
 export default function Counter() {
-<<<<<<< HEAD
+  console.log("RE-RENDERED");
   const [num, setNum] = useState(0);
-
   const changeNum = () => {
-    setNum(num + 1);
-    console.log("SET NUM HAS RUN!");
+    //When making a state related to the previous data always use callback!
+    setNum((current) => current + 1);
     console.log(`num: ${num}`);
+  };
+  const setToTen = () => {
+    setNum(10);
   };
 
   return (
     <div>
       <p>The count is : {num}</p>
       <button onClick={changeNum}>Increment</button>
+      <button onClick={setToTen}>Set to 10</button>
     </div>
   );
 }
