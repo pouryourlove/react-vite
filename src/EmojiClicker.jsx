687c103dc44e049bd9fefa59a62daff013266777
Juyeon Lee <lqu134@naver.com>
2023-06-13 22:04:36 +0900

    used UUID

--- src/EmojiClicker.jsx
+++ src/EmojiClicker.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 export default function EmojiClicker() {
-  const [emojis, setEmojis] = useState(["😂"]);
+  const [emojis, setEmojis] = useState([{ id: uuid(), emoji: "😭" }]);
   const addEmoji = () => {
-    setEmojis((oldEmojis) => [...emojis, "🥰"]);
+    setEmojis((oldEmojis) => [...emojis, { id: uuid(), emoji: "🥰" }]);
   };
   return (
     <div>
       {emojis.map((e) => (
-        <span style={{ fontSize: "4rem" }}>{e}</span>
+        <span key={e.id} style={{ fontSize: "4rem" }}>
+          {e.emoji}
+        </span>
       ))}
       <button onClick={addEmoji}>Add Emoji </button>
     </div>
