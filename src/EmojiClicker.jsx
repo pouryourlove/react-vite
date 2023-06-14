557f84619be819a34999997f70966887ac4f3afa
Juyeon Lee <lqu134@naver.com>
2023-06-14 15:07:59 +0900

    deleting from arrays

--- src/EmojiClicker.jsx
+++ src/EmojiClicker.jsx
@@ -1,15 +1,35 @@
 import { useState } from "react";
 import { v4 as uuid } from "uuid";
 
+function randomEmoji() {
+  const choices = ["😍", "🤣", "😘", "😆", "😥", "🤗"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
 export default function EmojiClicker() {
-  const [emojis, setEmojis] = useState([{ id: uuid(), emoji: "😭" }]);
+  const [emojis, setEmojis] = useState([{ id: uuid(), emoji: randomEmoji() }]);
   const addEmoji = () => {
-    setEmojis((oldEmojis) => [...emojis, { id: uuid(), emoji: "🥰" }]);
+    setEmojis((oldEmojis) => [
+      ...oldEmojis,
+      { id: uuid(), emoji: randomEmoji() },
+    ]);
+  };
+
+  //without mutating the array
+  const deleteEmoji = (id) => {
+    //delete the emoji with the specified id
+    setEmojis((prevEmojis) => {
+      return prevEmojis.filter((e) => e.id !== id);
+    });
   };
   return (
     <div>
       {emojis.map((e) => (
-        <span key={e.id} style={{ fontSize: "4rem" }}>
+        <span
+          onClick={() => deleteEmoji(e.id)}
+          key={e.id}
+          style={{ fontSize: "4rem" }}
+        >
           {e.emoji}
         </span>
       ))}
