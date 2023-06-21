d0a70e78dd5c111a8b59897db9b8700a1e26740f
Juyeon Lee <lqu134@naver.com>
2023-06-21 18:58:33 +0900

    lucky7

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,16 @@ import Counter from "./Counter";
 import Toggler from "./Toggler";
 import ToggleCounter from "./ToggleCounter";
 import ColorBox from "./ColorBox";
-import ColorBoxGrid from "./ColorBoxGrid";
+// import ColorBoxGrid from "./ColorBoxGrid";
 
-const properties = [
-  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
-  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
-  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
-  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
-  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
-  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
-];
+// const properties = [
+//   { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+//   { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+//   { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+//   { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+//   { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+//   { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+// ];
 
 // const data = [
 //   { id: 1, item: "eggs", quantity: 12, completed: false },
@@ -32,30 +32,30 @@ const properties = [
 //   { id: 3, item: "chicken breasts", quantity: 4, completed: false },
 //   { id: 4, item: "carrots", quantity: 6, completed: true },
 // ];
-const colors = [
-  "#E53935",
-  "#E91E63",
-  "#9C27B0",
-  "#673AB7",
-  "#3F51B5",
-  "#2196F3",
-  "#03A9F4",
-  "#00BCD4",
-  "#009688",
-  "#4CAF50",
-  "#8BC34A",
-  "#CDDC39",
-  "#FFEB3B",
-  "#FFC107",
-  "#FF9800",
-  "#FF5722",
-];
+// const colors = [
+//   "#E53935",
+//   "#E91E63",
+//   "#9C27B0",
+//   "#673AB7",
+//   "#3F51B5",
+//   "#2196F3",
+//   "#03A9F4",
+//   "#00BCD4",
+//   "#009688",
+//   "#4CAF50",
+//   "#8BC34A",
+//   "#CDDC39",
+//   "#FFEB3B",
+//   "#FFC107",
+//   "#FF9800",
+//   "#FF5722",
+// ];
 
 
 function App() {
   return (
     <div>
-      <ColorBoxGrid colors={colors}/>
+      {/* <ColorBoxGrid colors={colors}/> */}
       {/* <ColorBox colors = {colors}/> */}
       {/* <Counter/> */}
       {/* <ToggleCounter/> */}
